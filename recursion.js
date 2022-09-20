03d8f9abb63a7fdb37ec9170b2628e0a268f5088
Edmond Soun <edmond.soun@gmail.com>
2022-09-20 14:38:10 -0700

    solutions up to further study

--- recursion.js
+++ recursion.js
@@ -7,40 +7,50 @@ function product(nums) {
 }
 
 /** longest: return the length of the longest word in an array of words. */
-// Check length of each word against preceding word
-// Return greatest length
-// ["hi", "there", "wo"]
 
 function longest(words) {
-  if(words.length === 0) return 0;
+  if (words.length === 0) return 0;
 
   let wordlen = longest(words.slice(1));
 
   return Math.max(words[0].length, wordlen);
 
-  // if(words[0].length > wordlen){
-  //   return words[0].length;
-  // }else{
-  //   return wordlen;
-  // }
-
 }
 
 /** everyOther: return a string with every other letter. */
 
-function everyOther(str) {
+function everyOther(str, out = "", i = 0) {
+  if (str.length === i) return out;
 
+  if (i % 2 === 0) out += str[i];
+
+  return everyOther(str, out, i + 1);
 }
 
 /** find: return boolean depending on if val exists in array or not. */
 
-function find(arr, val) {
+function find(arr, val, i = 0) {
+
+  if (arr.length === i) return false;
+
+  if (arr[i] === val) {
+    return true;
+  } else {
+    return find(arr, val, i + 1)
+  }
 
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {
+function isPalindrome(str, i = 0) {
+  if (str.length / 2 < i) return true;
+
+  if (str[i] === str[str.length - 1 - i]) {
+    return isPalindrome(str, i + 1);
+  } else {
+    return false;
+  }
 
 }
 
@@ -48,53 +58,75 @@ function isPalindrome(str) {
 
 function revString(str) {
 
+  if (str[0] === undefined) return "";
+
+  return revString(str.slice(1)) + str[0]
+
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
+function findIndex(arr, val, i = 0) {
+  if (arr.length === i) return -1;
 
+  if (arr[i] === val) {
+    return i;
+  } else {
+    return findIndex(arr, val, i + 1)
+  }
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {
+function gatherStrings(obj, out = []) {
+
+
+  //if key has value that is string, push to array
+  for (let key in obj) {
+    if (typeof (obj[key]) === "string") {
+      out.push(obj[key]);
+    } else if (typeof (obj[key]) === "object") {
+      out = gatherStrings(obj[key], out)
+    }
+  }
+
+  return out;
 
 }
 
-// FURTHER STUDY
+  // FURTHER STUDY
 
-/** binarySearch: given a sorted array of numbers, and a value,
- * return true if val is in array, false if not present). */
+  /** binarySearch: given a sorted array of numbers, and a value,
+   * return true if val is in array, false if not present). */
 
-function binarySearch(arr, val) {
+  function binarySearch(arr, val) {
 
-}
-
-
-/** binarySearch: given a sorted array of numbers, and a value,
- * return the index of that value (or -1 if val is not present). */
-
-function binarySearchIndex(arr, val) {
-
-}
-
-// you might find the above two problems easier if you change the function signature to:
-//
-// function binarySearch(arr, val, left = 0, right = arr.length) {
-//
-// }
+  }
 
 
-module.exports = {
-  product,
-  longest,
-  everyOther,
-  find,
-  isPalindrome,
-  findIndex,
-  revString,
-  gatherStrings,
-  binarySearch,
-  binarySearchIndex,
-};
+  /** binarySearch: given a sorted array of numbers, and a value,
+   * return the index of that value (or -1 if val is not present). */
+
+  function binarySearchIndex(arr, val) {
+
+  }
+
+  // you might find the above two problems easier if you change the function signature to:
+  //
+  // function binarySearch(arr, val, left = 0, right = arr.length) {
+  //
+  // }
+
+
+  module.exports = {
+    product,
+    longest,
+    everyOther,
+    find,
+    isPalindrome,
+    findIndex,
+    revString,
+    gatherStrings,
+    binarySearch,
+    binarySearchIndex,
+  };
